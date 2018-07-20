df8b1149bffba583a598821ec13cf6b4749ae375
Cis <kalinca15@gmail.com>
2018-07-20 08:46:58 -0300

    inicio

--- app.js
+++ app.js
@@ -4,15 +4,22 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var swig = require('swig');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
+
 // view engine setup
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+
+app.set('view cache', false);
+// To disable Swig's cache, do the following:
+swig.setDefaults({ cache: false });
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
